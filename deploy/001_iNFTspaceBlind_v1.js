ca4a168b70d5f68af814ddcb91e1e3cd28228831
jason <panyongpeng@ystar.io>
2021-11-04 12:41:42 +0800

    add iNFTspaceBlind.

---
+++ deploy/001_iNFTspaceBlind_v1.js
@@ -0,0 +1,61 @@
+const {int} = require("hardhat/internal/core/params/argumentTypes");
+
+async function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('');
+    }, ms)
+  });
+}
+
+const func = async ({ getNamedAccounts, deployments, network }) => {
+  const { AddressZero } = ethers.constants;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  console.log('deployer is ', deployer)
+
+  const options = { from: deployer };
+
+  const mintWorkFee = "100000000000000000";  //0.1 ether;
+  const rewardThresholdWorks = 10;
+  const contractURI = "";
+  const tokenURIPrefix = "";
+
+  // Construction parameters
+  const params = [
+      mintWorkFee,
+      rewardThresholdWorks,
+      contractURI,
+      tokenURIPrefix,
+  ];
+
+  const infgspace = await deploy('iNFTspaceBlind', {...options, args: params});
+
+  if (network.live) {
+    signer = await ethers.getNamedSigner('deployer');
+  } else {
+    await network.provider.request({ method: "hardhat_impersonateAccount", params: [ signer ]});
+    signer = await ethers.getSigner(signer);
+  }
+
+  console.log('1. V1 iNFTspaceBlind has deployed at:', infgspace.address);
+
+  let waitTime = 400;
+  for (var i = 0; i< waitTime; i++){
+    await sleep(1000);
+    console.log('  wait deploy completed after', i, " s");
+  }
+  verifyAddress = infgspace.address;
+  // verifyAddress = '0xEd4aca02bC521641b6eDdCD1e3C7c404B5134404';
+  await run("verify:verify", {
+    address: verifyAddress,
+    constructorArguments: params
+  });
+  console.log('1. V1 iNFTspaceBlind has verifyed');
+
+  return network.live;
+};
+
+func.id = 'deploy_iNFTspaceBlind_v1';
+module.exports = func;
