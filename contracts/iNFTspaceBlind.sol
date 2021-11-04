503e1a4ce0ae55e68308f2f5353046b6cf27937d
jason <panyongpeng@ystar.io>
2021-11-04 15:40:46 +0800

    fix

--- contracts/iNFTspaceBlind.sol
+++ contracts/iNFTspaceBlind.sol
@@ -1071,7 +1071,7 @@ contract iNFTspaceBlind is Ownable, SignerRole, ERC1155Base {
     }
 
     function mint(uint256 id, uint8 v, bytes32 r, bytes32 s, Fee[] memory fees, uint256 value, string memory uri) public {
-        // require(isSigner(ecrecover(keccak256(abi.encodePacked(this, id)), v, r, s)), "signer should sign tokenId");
+        // require(isSigner(ecrecover(keccak256(abi.encodePacked(this, id, value, uri)), v, r, s)), "signer should sign tokenId");
         require(minters[msg.sender].remainMintWorks >= value, "mint remain time is require");
 
         _mint(baseMinter, msg.sender,  id, fees, value, uri);
