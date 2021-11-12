fd6a7b36089a7d6cd814e210593fd772ff2f0b95
jason <panyongpeng@ystar.io>
2021-11-12 18:39:44 +0800

    fix

--- deploy/001_iNFTspaceV1.js
+++ deploy/001_iNFTspaceV1.js
@@ -37,13 +37,13 @@ const func = async ({ getNamedAccounts, deployments, network }) => {
 
   await blind.deployed();
 
-  // let waitTime = 60; // 60 s
-  // for (var i = 0; i< waitTime; i++){
-  //   await sleep(1000);
-  //   if ( i%3 == 0) {
-  //     console.log('  wait deploy completed after', waitTime - i, " s");
-  //   }
-  // }
+  let waitTime = 30; // 30 s wait scan indexed
+  for (var i = 0; i< waitTime; i++){
+    await sleep(1000);
+    if ( i%3 == 0) {
+      console.log('  wait deploy completed after', waitTime - i, " s");
+    }
+  }
 
   verifyAddress = infgspace.address;
   // verifyAddress = '0xEd4aca02bC521641b6eDdCD1e3C7c404B5134404';
